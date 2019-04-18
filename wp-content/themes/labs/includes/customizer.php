ae4bf3a63a088a0239c10c775b95e4aba6ac60fc
Christopher Nisengwe <nisengwe.christopher@gmail.com>
2019-04-18 16:27:31 +0200

    11. add_control pour les images dans le caroussel sont fait.

--- wp-content/themes/labs/includes/customizer.php
+++ wp-content/themes/labs/includes/customizer.php
@@ -15,30 +15,57 @@ add_theme_support( 'custom-logo', array(
  * Pour modifier le texte dans le menus en haut de page et voir changements dans les 'templates : page_header.php'selon 
  */
  class labsCustomizer {
+
      /**
-      * Personnalisation dans le header
+      * 
+      * Personnalisation dans le carroussel, le logo et les images.
+      * La page de reference == templates/intro_section.php.
+      *
       */
-
     static public function modification_header($wp_customize) {
-
+        // Panel - personnalisation dans le fichier intro_section.php
         $wp_customize->add_panel('panel-header',[
             'title' => __('Modification Carrousel'),
             'description' => __('Personnalisation du Caroussel'),
         ]);
-
+        
+        // Sections de personnalisation pour le logo et les images dans intro_section.php
         $wp_customize->add_section('section-header-logo',[
             'panel' => 'panel-header',
-            'title' => __('Personnalisation logo'),
+            'title' => __('Personnalisation du logo caroussel'),
+            'description' => __('Modification du logo peut se faire par une image / du texte'),
+        ]);
+        $wp_customize->add_section('section-header-images',[
+            'panel' => 'panel-header',
+            'title' => __('Personnalisation des images caroussel'),
             'description' => __('Modification du logo peut se faire par une image / du texte'),
         ]);
 
-        
-        $wp_customize->add_setting('setting-header-logo');
 
+        // Setting personnalisation du logo et des images du fichier intro_section.php
+        $wp_customize->add_setting('setting-header-logo');
+        $wp_customize->add_setting('setting-header-image-1');
+        $wp_customize->add_setting('setting-header-image-2');
+        
+
+
+        // Controls personnalisation du logo et des images du fichier intro_Section.php
         $wp_customize->add_control(new WP_Customize_Image_Control (
             $wp_customize, 'setting-header-logo', [
                 'section' => 'section-header-logo',
-                'label' => __('Modification du texte'),
+                'label' => __('Modification du logo'),
+            ]
+        ));
+        $wp_customize->add_control(new WP_Customize_Image_Control (
+            $wp_customize, 'setting-header-image-1', [
+                'section' => 'section-header-images',
+                'label' => __('Modification de l\'image n°1 du caroussel' ),
+            ]
+        ));
+        $wp_customize->add_control(new WP_Customize_Image_Control (
+            $wp_customize, 'setting-header-image-2', [
+                'section' => 'section-header-images',
+                'label' => __('Modification de l\'image n°2 du caroussel' ),
             ]
         ));
 
--- wp-content/themes/labs/templates/intro_section.php
+++ wp-content/themes/labs/templates/intro_section.php
@@ -8,8 +8,8 @@
     </div>
     <!-- slider -->
     <div id="hero-slider" class="owl-carousel">
-      <div class="item  hero-item" data-bg="<?= LABS_IMG . '01.jpg'?>"></div>
-      <div class="item  hero-item" data-bg="<?= LABS_IMG . '02.jpg'?>"></div>
+      <div class="item  hero-item" data-bg="<?= get_theme_mod('setting-header-image-1') ?>"></div>
+      <div class="item  hero-item" data-bg="<?= get_theme_mod('setting-header-image-2') ?>"></div>
     </div>
   </div>
   <!-- Intro Section -->
