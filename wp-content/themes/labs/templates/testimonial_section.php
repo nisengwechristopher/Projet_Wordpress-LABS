4f7e242d84c61bce36a3e21f74e24ccc14c83a3a
Christopher Nisengwe <nisengwe.christopher@gmail.com>
2019-04-21 21:45:46 +0200

    15.1 Suppression de deux phrases test.

--- wp-content/themes/labs/templates/testimonial_section.php
+++ wp-content/themes/labs/templates/testimonial_section.php
@@ -6,9 +6,6 @@
         <div class="col-md-8 col-md-offset-4">
           <div class="section-title left">
             <h2><?= get_theme_mod('setting-testimonial-titre');?></h2>
-            <p><?= get_template_directory_uri(); ?></p>
-            <p><?= get_template_directory(); ?></p>
-
           </div>
           <div class="owl-carousel" id="testimonial-slide">
             <!-- single testimonial -->
