233ba9e470166502c63fbd0d674a4b44cadba5c8
Christopher Nisengwe <nisengwe.christopher@gmail.com>
2019-04-25 12:48:03 +0200

    19. Dansl le fichier about_template.php j'ai reglé le problème avec l'affichage des random posts on céeant les colones dynamiquement.

--- wp-content/themes/labs/templates/about_section.php
+++ wp-content/themes/labs/templates/about_section.php
@@ -2,108 +2,37 @@
   <div class="about-section">
     <div class="overlay"></div>
     <!-- card section -->
-    <div class="card-section">
-      <div class="container">
-        <div class="row">
-          <!-- single card -->
-          <div class="col-md-4 col-sm-6">
-            <div class="lab-card">
-              <div class="icon">
-                <i class="flaticon-023-flask"></i>
-              </div>
-              <!-- Recupération de manière dynamique des postes afin des les afficher.!! -->
-                <?php
-                // Query random posts
-                $the_query = new WP_Query( array(
-                  'post_type'      => 'post',
-                  'orderby'        => 'rand',
-                  'posts_per_page' => 1,
-                  'category_name' => 'services', // permet de vérifier la catégorie 
-                ) );
+    <!-- Recupération de manière dynamique des postes afin des les afficher.!! -->
+      <div class="card-section">
+        <div class="container">
+          <div class="row">
+            <!-- single card -->
+            <?php
+            // Query random posts
+            $the_query = new WP_Query( array(
+              'post_type'      => 'post',
+              'orderby'        => 'rand',
+              'posts_per_page' => 3,
+              'category_name' => 'services', // permet de vérifier la catégorie 
+            ) );
 
-                // If we have posts lets show them
-                if ( $the_query->have_posts() ) : ?>
-                  <div id="randomposts">
-                    <ul>
-                      <?php
-                      // Loop through the posts
-                      while ( $the_query->have_posts() ) : $the_query->the_post();  ?>
+            // If we have posts lets show them
+            if ( $the_query->have_posts() ) : ?>
+                  <?php
+                  // Loop through the posts
+                  while ( $the_query->have_posts() ) : $the_query->the_post();  ?>
+                    <div class="col-md-4 col-sm-6">
+                      <div class="lab-card">
+                        <div class="icon">
+                          <i class="flaticon-023-flask"></i>
+                        </div>
                         <h2><a class="post_links" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
                         <p><?= the_content() ?></p>
-
-                      <?php endwhile; ?>
-                      <?php wp_reset_postdata(); ?>
-                    </ul>
-                  </div>
-
-                <?php endif;?>
-            </div>
-          </div>
-          <!-- single card -->
-          <div class="col-md-4 col-sm-6">
-            <div class="lab-card">
-              <div class="icon">
-                <i class="flaticon-011-compass"></i>
-              </div>
-              <!-- Recupération de manière dynamique des postes afin des les afficher.!! faut mettre une condition pour verifier la Catégorie. -->
-                <?php
-                // Query random posts
-                $the_query = new WP_Query( array(
-                  'post_type'      => 'post',
-                  'orderby'        => 'rand',
-                  'posts_per_page' => 1,
-                  'category_name' => 'services', // permet de vérifier la catégorie 
-                ) );
-
-                // If we have posts lets show them
-                if ( $the_query->have_posts() ) : ?>
-
-                  <div id="randomposts">
-                    <ul>
-                      <?php
-                      // Loop through the posts
-                      while ( $the_query->have_posts() ) : $the_query->the_post();  ?>
-                        <h2><a class="post_links" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-                        <p><?= the_content()?></p>
-                      <?php endwhile; ?>
-                      <?php wp_reset_postdata(); ?>
-                    </ul>
-                  </div>
-                <?php endif;?>
-            </div>
-          </div>
-          <!-- single card -->
-          <div class="col-md-4 col-sm-12">
-            <div class="lab-card">
-              <div class="icon">
-                <i class="flaticon-037-idea"></i>
-              </div>
-            <!-- Recupération de manière dynamique des postes afin des les afficher.!! faut mettre une condition pour verifier la Catégorie. -->
-                <?php
-                // Query random posts
-                $the_query = new WP_Query( array(
-                  'post_type'      => 'post',
-                  'orderby'        => 'rand',
-                  'posts_per_page' => 1,
-                  'category_name' => 'services', // permet de vérifier la catégorie 
-                ) );
-
-                // If we have posts lets show them
-                if ( $the_query->have_posts() ) : ?>
-                  <div id="randomposts">
-                    <ul>
-                      <?php
-                      // Loop through the posts
-                      while ( $the_query->have_posts() ) : $the_query->the_post();  ?>
-                        <h2><a class="post_links" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-                        <p><?= the_content() ?></p>
-                      <?php endwhile; ?>
-                      <?php wp_reset_postdata(); ?>
-                    </ul>
-                  </div>
-                <?php endif;?>
-            </div>
-          </div>
+                      </div>
+                    </div>
+                  <?php endwhile;
+                  endif ?>
+                  <?php wp_reset_postdata(); ?>
         </div>
       </div>
     </div>
